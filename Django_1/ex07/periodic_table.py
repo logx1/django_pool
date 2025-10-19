68ce8e77ec04cb82f494f26b1a1cfcc3d66e2f0b
Abdelmajid El-oualy <abdel-ou@c1r7p2.1337.ma>
2025-10-19 14:02:40 +0100

    django day1

--- Django_1/ex07/periodic_table.py
+++ Django_1/ex07/periodic_table.py
@@ -1,78 +1,76 @@
-import sys
+def line_to_dict(line):
+    line = line.strip("\n")
+    periodic_case = {}
+    new_line = line.split("=")
 
-def generate_periodic_table(periodic_data):
-    periodic_html = """"""
-    case_index = 0
-    for elem, infos in periodic_data:
-        if infos['position'] == "0":
-            periodic_html += " <tr>\n"
-        while case_index < int(infos['position']):
-            periodic_html += """<td>&nbsp;</td>"""
-            case_index += 1
-        periodic_html += """
-            <td style="border: 1px solid black; padding: 10px;">
-                <h4>%(name)s</h4>
-                <h1>%(small)s</h1>
-                <ul>
-                    <li>No %(number)s</li>
-                    <li>%(molar)s</li>
-                    <li>%(electron)s electron(s)</li>
-                </ul>
-            </td>
-        """ % infos
-        case_index += 1
-        if infos['position'] == "17":
-            periodic_html += " </tr>"
-            case_index = 0
-    return periodic_html
+    periodic_case['element'] = new_line[0]
+    periodic_case[new_line[1].split(",")[0].split(":")[0]] = new_line[1].split(",")[0].split(":")[1]
+    periodic_case[new_line[1].split(",")[1].split(":")[0]] = new_line[1].split(",")[1].split(":")[1]
+    periodic_case[new_line[1].split(",")[2].split(":")[0]] = new_line[1].split(",")[2].split(":")[1]
+    periodic_case[new_line[1].split(",")[3].split(":")[0]] = new_line[1].split(",")[3].split(":")[1]
+    periodic_case[new_line[1].split(",")[4].split(":")[0]] = new_line[1].split(",")[4].split(":")[1]
+    # print(periodic_case)
+    return periodic_case
 
-def generate_template(periodic_data):
-    html_string = """
-    <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Periodic table</title>
-            <meta charset='UTF-8'>
-            <style>
-                td {
-                    border: 1px solid black;
-                    padding: 10px;
-                    display: block;
-                    float: left;
-                }
-            </style>
-        </head>
-        <body>
-            %s
-        </body>
-    <html>
-    """
-    table_template = """
-                <table style='empty-cells: show;'>
-                    %s
-                </table>
-    """
-    return(table_template % generate_periodic_table(periodic_data))
+def read_file(file):
+    periodic_table = []
+    with open(file, "r") as fd:
+        for line in fd:
+            elem_dict = line_to_dict(line)
+            if elem_dict:
+                periodic_table.append(elem_dict)
+    return periodic_table
 
-def run():
-    periodic_data = []
-    with open('periodic_table.txt', 'r') as fd:
-        for line in fd.readlines():
-            key, value = line.split('=')
-            infos = value.split(',')
-            periodic_data.append(
-                (key, {
-                    'name': key,
-                    'position': infos[0].split(':')[1],
-                    'number': infos[1].split(':')[1],
-                    'small': infos[2].split(':')[1],
-                    'molar': infos[3].split(':')[1],
-                    'electron': sum(map(int, infos[4].split(':')[1].replace('\n', '').split(' '))),
-                })
-            )
-    html_string = generate_template(periodic_data)
-    with open('periodic_table.html', 'w+') as fd:
-        fd.write(html_string)
+def periodic_table_to_html(periodic_table):
+    html = """<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Periodic Table</title>
+<style>
+body { font-family: Arial, sans-serif; margin: 20px; }
+.periodic-grid {
+    display: grid;
+    grid-template-columns: repeat(18, 60px);
+    grid-auto-rows: 80px;
+    gap: 5px;
+}
+.element {
+    border: 1px solid #333;
+    padding: 5px;
+    text-align: center;
+    font-size: 12px;
+    background-color: #f2f2f2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.element span.symbol { font-size: 18px; font-weight: bold; }
+.element span.number { font-size: 10px; color: #555; }
+</style>
+</head>
+<body>
+<h1>Periodic Table</h1>
+<div class="periodic-grid">
+"""
+    for elem in periodic_table:
+        col = int(elem.get('position', 0)) + 1  # Grid column starts from 1
+        html += f'<div class="element" style="grid-column:{col}">'
+        html += f'<span class="symbol">{elem.get("small","")}</span>'
+        html += f'<span class="number">{elem.get("number","")}</span>'
+        html += f'<span class="name">{elem.get("element","")}</span>'
+        html += f'<span class="molar">{elem.get("molar","")}</span>'
+        html += f'</div>\n'
 
-if __name__ == '__main__':
-    run()
+    html += "</div>\n</body>\n</html>"
+    return html
+
+if __name__ == "__main__":
+    table = read_file("periodic_table.txt")
+    html_content = periodic_table_to_html(table)
+
+    with open("periodic_table.html", "w", encoding="utf-8") as f:
+        f.write(html_content)
+
+    print("Periodic table HTML file created as 'periodic_table.html'.")
