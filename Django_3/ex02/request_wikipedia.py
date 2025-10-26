e1a486f4509e4b6ab41e12e05019f9959da04600
Abdelmajid El-oualy <abdel-ou@c1r8p8.1337.ma>
2025-10-26 20:41:24 +0100

    try with ex02 in day 02

--- Django_3/ex02/request_wikipedia.py
+++ Django_3/ex02/request_wikipedia.py
@@ -26,6 +26,7 @@ class WikiApi:
         if isinstance(search, str) and search.strip():
             self.filename = "_".join(search.split()) + ".wiki"
             self.search_params['srsearch'] = search.lower()
+            self.page_ids = []
         else:
             print("You must specify something to search.\n")
             return
@@ -36,6 +37,11 @@ class WikiApi:
             print("Error: Status code %s (%s)" % (res.status_code, res.reason))
             return None
         return res.json()
+
+    def get_page_ids(self, search_result):
+        for el in search_result["query"]["search"]:
+            self.page_ids.append(el["pageid"])
+        print(self.page_ids)
 
     def get_page_extract(self, pageid):
         params = {
@@ -55,21 +61,28 @@ class WikiApi:
 
 
 if __name__ == "__main__":
-    # if len(sys.argv) == 2:
-    #     test = WikiApi(sys.argv[1]).search()
+    if len(sys.argv) == 2:
+        test = WikiApi(sys.argv[1])
 
-    #     if len(test["query"]["search"]) == 0:
-    #         print("no rusolt fro wiki ")
-    #     else:
-    #         print(json.dumps(test["query"]["search"][0]["pageid"], indent=4))
-    #     page = WikiApi("chocolatine").get_page_extract(test["query"]["search"][0]["pageid"])
-    #     print(page)
-    # else:
-    #     print("ERRor the correct : python request_wikipedia.py keyword")
-    test = WikiApi(sys.argv[1]).search()
-    for el in test["query"]["search"]:
-        print(json.dumps(el["pageid"],indent=4))
-    print("=========================================================")  
-    print(json.dumps(test, indent=4))
+        if len(test.search()["query"]["search"]) == 0:
+            print("no rusolt fro wiki ")
+        else:
+            print(json.dumps(test.search()["query"]["search"][0]["pageid"], indent=4))
+            print("\n the length of extract is : " , len(test.search()["query"]["search"]))
+            list_of_page_ids = []
+            for el in test.search()["query"]["search"]:
+                list_of_page_ids.append(el["pageid"])
+                print("--------------------------------------------------\n")
+        page = WikiApi("chocolatine").get_page_extract(test.search()["query"]["search"][0]["pageid"])
+        print(page)
+        # print(list_of_page_ids)
+        
+    else:
+        print("ERRor the correct : python request_wikipedia.py keyword")
+    # test = WikiApi(sys.argv[1]).search()
+    # for el in test["query"]["search"]:
+    #     print(json.dumps(el["pageid"],indent=4))
+    # print("=========================================================")  
+    # print(json.dumps(test, indent=4))
 
         
